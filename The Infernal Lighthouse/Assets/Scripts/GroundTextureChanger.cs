498c6faa79218ae7e6cd3805711c20c45733cf09
PhilipKuzin <kuzinphilipp@gmail.com>
2024-08-21 17:22:54 +0300

    environment of the main scene has been changed. ready to further programming

---
+++ The Infernal Lighthouse/Assets/Scripts/GroundTextureChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTextureChanger : MonoBehaviour
+{
+    [SerializeField] private Material [] _groundMaterials;  
+
+    private void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+
+        if (_groundMaterials.Length > 0)
+        {
+            int randomMaterial = Random.Range(0, _groundMaterials.Length);
+            renderer.material = _groundMaterials[randomMaterial];
+        }
+    }
+
+
+}
---
+++ The Infernal Lighthouse/Assets/Scripts/Player/RaycastAttak.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastAttak 
+{
+    public event Action OnEnemyKilled;      // добавлено 25.06! событие убийства врага
+
+    private ParticleService _particleService;
+
+    public RaycastAttak(ParticleService particleService)
+    {
+        _particleService = particleService;
+    }
+
+    public void PerformAttack(Vector3 position)  // потенциально добавл€ем флаг разброса (дробовик) и дополн€ем логику примером от night train code 
+    {
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            Collider hitCollider = hitInfo.collider;
+
+            if (hitCollider.TryGetComponent(out IDamageable enemy) && enemy is Enemy realEnemy) // партиклы на ма€ке по€вл€ютс€ из-за того что ма€к тоже IDamageable (ѕќƒ”ћј“№!) проверка IS ? 
+            {
+                OnEnemyKilled?.Invoke();                            // добавлено 25.06! вызов событи€ убийства врага! 
+                _particleService.SpawnParticleEffectExplosion(hitInfo);
+                realEnemy.TakeDamage();
+            }
+            else
+            {
+                _particleService.SpawnParticleEffectMiss(hitInfo);
+            }
+        }
+    }
+}
